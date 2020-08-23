8f900750a4d8c6349ac551dad37ebc3420fa25f6
joshkenney <joshuagkenney@gmail.com>
2020-08-22 22:38:32 -0400

    refactor exp/fn.js

--- js/playTime.js
+++
@@ -1,7 +0,0 @@
-function playTime(){
-    if (MinutesToPlay == 20) {
-        alert("Thank you! You will play the experiment for "+MinutesToPlay +" minutes.")
-    } else {
-        alert("You did not enter a number, please refresh the browser and try again.");
-    }
-}
